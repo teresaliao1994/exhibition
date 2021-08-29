bd39f1ce884c1ea696425cebc5b5fc9bb7ad53ba
Pinjung Liao <pinjung.liao@gmail.com>
2021-08-29 16:36:26 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,21 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+//Aos
+AOS.init({
+  once: true,
+});
+
+//swipers
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 });
