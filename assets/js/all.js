b834c78a4b68fbdd2651efd3d259c84135570778
Pinjung Liao <pinjung.liao@gmail.com>
2021-08-30 16:24:19 +0800

    Update 2021-08-30T08:24:05.955Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,19 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+//Aos
+AOS.init({
+  once: true
+}); //swipers
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  grabCursor: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
